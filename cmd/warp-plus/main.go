e01373ff6512ceaa0513f29b1af31d95f1955149
Mark Pashmfouroush <mark@markpash.me>
2025-02-26 22:40:22 +0000

    migrate to ffcommand

--- cmd/warp-plus/main.go
+++ cmd/warp-plus/main.go
@@ -5,20 +5,10 @@ import (
 	"errors"
 	"fmt"
 	"log/slog"
-	"net/netip"
 	"os"
 	"os/signal"
-	"path"
 	"syscall"
-	"time"
 
-	"github.com/adrg/xdg"
-	"github.com/bepass-org/warp-plus/app"
-	p "github.com/bepass-org/warp-plus/psiphon"
-	"github.com/bepass-org/warp-plus/warp"
-	"github.com/bepass-org/warp-plus/wiresocks"
-
-	"github.com/carlmjohnson/versioninfo"
 	"github.com/peterbourgon/ff/v4"
 	"github.com/peterbourgon/ff/v4/ffhelp"
 	"github.com/peterbourgon/ff/v4/ffjson"
@@ -29,130 +19,28 @@ const appName = "warp-plus"
 var version string = ""
 
 func main() {
-	fs := ff.NewFlagSet(appName)
-	var (
-		v4       = fs.BoolShort('4', "only use IPv4 for random warp endpoint")
-		v6       = fs.BoolShort('6', "only use IPv6 for random warp endpoint")
-		verbose  = fs.Bool('v', "verbose", "enable verbose logging")
-		bind     = fs.String('b', "bind", "127.0.0.1:8086", "socks bind address")
-		endpoint = fs.String('e', "endpoint", "", "warp endpoint")
-		key      = fs.String('k', "key", "", "warp key")
-		dns      = fs.StringLong("dns", "1.1.1.1", "DNS address")
-		gool     = fs.BoolLong("gool", "enable gool mode (warp in warp)")
-		psiphon  = fs.BoolLong("cfon", "enable psiphon mode (must provide country as well)")
-		country  = fs.StringEnumLong("country", fmt.Sprintf("psiphon country code (valid values: %s)", p.Countries), p.Countries...)
-		scan     = fs.BoolLong("scan", "enable warp scanning")
-		rtt      = fs.DurationLong("rtt", 1000*time.Millisecond, "scanner rtt limit")
-		cacheDir = fs.StringLong("cache-dir", "", "directory to store generated profiles")
-		fwmark   = fs.UintLong("fwmark", 0x0, "set linux firewall mark for tun mode (requires sudo/root/CAP_NET_ADMIN)")
-		reserved = fs.StringLong("reserved", "", "override wireguard reserved value (format: '1,2,3')")
-		wgConf   = fs.StringLong("wgconf", "", "path to a normal wireguard config")
-		testUrl  = fs.StringLong("test-url", "http://connectivity.cloudflareclient.com/cdn-cgi/trace", "connectivity test url")
-		_        = fs.String('c', "config", "", "path to config file")
-		verFlag  = fs.BoolLong("version", "displays version number")
-	)
-
-	err := ff.Parse(
-		fs,
-		os.Args[1:],
+	args := os.Args[1:]
+	ctx, _ := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	rootCmd := newRootCmd()
+	err := rootCmd.command.Parse(
+		args,
 		ff.WithConfigFileFlag("config"),
 		ff.WithConfigFileParser(ffjson.Parse),
 	)
+
 	switch {
 	case errors.Is(err, ff.ErrHelp):
-		fmt.Fprintf(os.Stderr, "%s\n", ffhelp.Flags(fs))
+		fmt.Fprintf(os.Stderr, "%s\n", ffhelp.Flags(rootCmd.flags))
 		os.Exit(0)
 	case err != nil:
 		fmt.Fprintf(os.Stderr, "error: %v\n", err)
 		os.Exit(1)
 	}
 
-	if *verFlag {
-		if version == "" {
-			version = versioninfo.Short()
-		}
-		fmt.Fprintf(os.Stderr, "%s\n", version)
-		os.Exit(0)
+	if err := rootCmd.command.Run(ctx); err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(1)
 	}
-
-	l := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
-
-	if *verbose {
-		l = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
-	}
-
-	if *psiphon && *gool {
-		fatal(l, errors.New("can't use cfon and gool at the same time"))
-	}
-
-	if *v4 && *v6 {
-		fatal(l, errors.New("can't force v4 and v6 at the same time"))
-	}
-
-	if !*v4 && !*v6 {
-		*v4, *v6 = true, true
-	}
-
-	bindAddrPort, err := netip.ParseAddrPort(*bind)
-	if err != nil {
-		fatal(l, fmt.Errorf("invalid bind address: %w", err))
-	}
-
-	dnsAddr, err := netip.ParseAddr(*dns)
-	if err != nil {
-		fatal(l, fmt.Errorf("invalid DNS address: %w", err))
-	}
-
-	opts := app.WarpOptions{
-		Bind:            bindAddrPort,
-		Endpoint:        *endpoint,
-		License:         *key,
-		DnsAddr:         dnsAddr,
-		Gool:            *gool,
-		FwMark:          uint32(*fwmark),
-		WireguardConfig: *wgConf,
-		Reserved:        *reserved,
-		TestURL:         *testUrl,
-	}
-
-	switch {
-	case *cacheDir != "":
-		opts.CacheDir = *cacheDir
-	case xdg.CacheHome != "":
-		opts.CacheDir = path.Join(xdg.CacheHome, appName)
-	case os.Getenv("HOME") != "":
-		opts.CacheDir = path.Join(os.Getenv("HOME"), ".cache", appName)
-	default:
-		opts.CacheDir = "warp_plus_cache"
-	}
-
-	if *psiphon {
-		l.Info("psiphon mode enabled", "country", *country)
-		opts.Psiphon = &app.PsiphonOptions{Country: *country}
-	}
-
-	if *scan {
-		l.Info("scanner mode enabled", "max-rtt", rtt)
-		opts.Scan = &wiresocks.ScanOptions{V4: *v4, V6: *v6, MaxRTT: *rtt}
-	}
-
-	// If the endpoint is not set, choose a random warp endpoint
-	if opts.Endpoint == "" {
-		addrPort, err := warp.RandomWarpEndpoint(*v4, *v6)
-		if err != nil {
-			fatal(l, err)
-		}
-		opts.Endpoint = addrPort.String()
-	}
-
-	ctx, _ := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
-	go func() {
-		if err := app.RunWarp(ctx, l, opts); err != nil {
-			fatal(l, err)
-		}
-	}()
-
-	<-ctx.Done()
 }
 
 func fatal(l *slog.Logger, err error) {
---
+++ cmd/warp-plus/rootcmd.go
@@ -0,0 +1,241 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"log/slog"
+	"net/netip"
+	"os"
+	"path"
+	"time"
+
+	"github.com/adrg/xdg"
+	"github.com/bepass-org/warp-plus/app"
+	p "github.com/bepass-org/warp-plus/psiphon"
+	"github.com/bepass-org/warp-plus/warp"
+	"github.com/bepass-org/warp-plus/wiresocks"
+	"github.com/carlmjohnson/versioninfo"
+	"github.com/peterbourgon/ff/v4"
+	"github.com/peterbourgon/ff/v4/ffval"
+)
+
+type rootConfig struct {
+	flags   *ff.FlagSet
+	command *ff.Command
+
+	verbose  bool
+	v4       bool
+	v6       bool
+	bind     string
+	endpoint string
+	key      string
+	dns      string
+	gool     bool
+	psiphon  bool
+	country  string
+	scan     bool
+	rtt      time.Duration
+	cacheDir string
+	fwmark   uint32
+	reserved string
+	wgConf   string
+	testUrl  string
+	config   string
+	version  bool
+}
+
+func newRootCmd() *rootConfig {
+	var cfg rootConfig
+	cfg.flags = ff.NewFlagSet(appName)
+	cfg.flags.AddFlag(ff.FlagConfig{
+		ShortName: 'v',
+		LongName:  "verbose",
+		Value:     ffval.NewValueDefault(&cfg.verbose, false),
+		Usage:     "enable verbose logging",
+		NoDefault: true,
+	})
+	cfg.flags.AddFlag(ff.FlagConfig{
+		ShortName: '4',
+		Value:     ffval.NewValueDefault(&cfg.v4, false),
+		Usage:     "only use IPv4 for random warp endpoint",
+	})
+	cfg.flags.AddFlag(ff.FlagConfig{
+		ShortName: '6',
+		Value:     ffval.NewValueDefault(&cfg.v6, false),
+		Usage:     "only use IPv6 for random warp endpoint",
+	})
+	cfg.flags.AddFlag(ff.FlagConfig{
+		ShortName: 'b',
+		LongName:  "bind",
+		Value:     ffval.NewValueDefault(&cfg.bind, "127.0.0.1:8086"),
+		Usage:     "socks bind address",
+	})
+	cfg.flags.AddFlag(ff.FlagConfig{
+		ShortName: 'e',
+		LongName:  "endpoint",
+		Value:     ffval.NewValueDefault(&cfg.endpoint, ""),
+		Usage:     "warp endpoint",
+	})
+	cfg.flags.AddFlag(ff.FlagConfig{
+		ShortName: 'k',
+		LongName:  "key",
+		Value:     ffval.NewValueDefault(&cfg.key, ""),
+		Usage:     "warp key",
+	})
+	cfg.flags.AddFlag(ff.FlagConfig{
+		LongName: "dns",
+		Value:    ffval.NewValueDefault(&cfg.dns, "1.1.1.1"),
+		Usage:    "DNS address",
+	})
+	cfg.flags.AddFlag(ff.FlagConfig{
+		LongName: "gool",
+		Value:    ffval.NewValueDefault(&cfg.gool, false),
+		Usage:    "enable gool mode (warp in warp)",
+	})
+	cfg.flags.AddFlag(ff.FlagConfig{
+		LongName: "cfon",
+		Value:    ffval.NewValueDefault(&cfg.psiphon, false),
+		Usage:    "enable psiphon mode (must provide country as well)",
+	})
+	cfg.flags.AddFlag(ff.FlagConfig{
+		LongName: "country",
+		Value:    ffval.NewEnum(&cfg.country, p.Countries...),
+		Usage:    "psiphon country code",
+	})
+	cfg.flags.AddFlag(ff.FlagConfig{
+		LongName: "scan",
+		Value:    ffval.NewValueDefault(&cfg.scan, false),
+		Usage:    "enable warp scanning",
+	})
+	cfg.flags.AddFlag(ff.FlagConfig{
+		LongName: "rtt",
+		Value:    ffval.NewValueDefault(&cfg.rtt, 1000*time.Millisecond),
+	})
+	cfg.flags.AddFlag(ff.FlagConfig{
+		LongName: "cache-dir",
+		Value:    ffval.NewValueDefault(&cfg.cacheDir, ""),
+	})
+	cfg.flags.AddFlag(ff.FlagConfig{
+		LongName: "fwmark",
+		Value:    ffval.NewValueDefault(&cfg.fwmark, 0x0),
+	})
+	cfg.flags.AddFlag(ff.FlagConfig{
+		LongName: "reserved",
+		Value:    ffval.NewValueDefault(&cfg.reserved, ""),
+	})
+	cfg.flags.AddFlag(ff.FlagConfig{
+		LongName: "wgconf",
+		Value:    ffval.NewValueDefault(&cfg.wgConf, ""),
+	})
+	cfg.flags.AddFlag(ff.FlagConfig{
+		LongName: "test-url",
+		Value:    ffval.NewValueDefault(&cfg.testUrl, "http://connectivity.cloudflareclient.com/cdn-cgi/trace"),
+	})
+	cfg.flags.AddFlag(ff.FlagConfig{
+		ShortName: 'c',
+		LongName:  "config",
+		Value:     ffval.NewValueDefault(&cfg.config, ""),
+	})
+	cfg.flags.AddFlag(ff.FlagConfig{
+		LongName: "version",
+		Value:    ffval.NewValueDefault(&cfg.version, false),
+		Usage:    "displays version",
+	})
+	cfg.command = &ff.Command{
+		Name:  appName,
+		Flags: cfg.flags,
+		Exec:  cfg.exec,
+	}
+	return &cfg
+}
+
+func (c *rootConfig) exec(ctx context.Context, args []string) error {
+	if c.version {
+		if version == "" {
+			version = versioninfo.Short()
+		}
+		fmt.Fprintf(os.Stderr, "%s\n", version)
+		os.Exit(0)
+	}
+
+	l := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+
+	if c.verbose {
+		l = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	}
+
+	if c.psiphon && c.gool {
+		fatal(l, errors.New("can't use cfon and gool at the same time"))
+	}
+
+	if c.v4 && c.v6 {
+		fatal(l, errors.New("can't force v4 and v6 at the same time"))
+	}
+
+	if !c.v4 && !c.v6 {
+		c.v4, c.v6 = true, true
+	}
+
+	bindAddrPort, err := netip.ParseAddrPort(c.bind)
+	if err != nil {
+		fatal(l, fmt.Errorf("invalid bind address: %w", err))
+	}
+
+	dnsAddr, err := netip.ParseAddr(c.dns)
+	if err != nil {
+		fatal(l, fmt.Errorf("invalid DNS address: %w", err))
+	}
+
+	opts := app.WarpOptions{
+		Bind:            bindAddrPort,
+		Endpoint:        c.endpoint,
+		License:         c.key,
+		DnsAddr:         dnsAddr,
+		Gool:            c.gool,
+		FwMark:          c.fwmark,
+		WireguardConfig: c.wgConf,
+		Reserved:        c.reserved,
+		TestURL:         c.testUrl,
+	}
+
+	switch {
+	case c.cacheDir != "":
+		opts.CacheDir = c.cacheDir
+	case xdg.CacheHome != "":
+		opts.CacheDir = path.Join(xdg.CacheHome, appName)
+	case os.Getenv("HOME") != "":
+		opts.CacheDir = path.Join(os.Getenv("HOME"), ".cache", appName)
+	default:
+		opts.CacheDir = "warp_plus_cache"
+	}
+
+	if c.psiphon {
+		l.Info("psiphon mode enabled", "country", c.country)
+		opts.Psiphon = &app.PsiphonOptions{Country: c.country}
+	}
+
+	if c.scan {
+		l.Info("scanner mode enabled", "max-rtt", c.rtt)
+		opts.Scan = &wiresocks.ScanOptions{V4: c.v4, V6: c.v6, MaxRTT: c.rtt}
+	}
+
+	// If the endpoint is not set, choose a random warp endpoint
+	if opts.Endpoint == "" {
+		addrPort, err := warp.RandomWarpEndpoint(c.v4, c.v6)
+		if err != nil {
+			fatal(l, err)
+		}
+		opts.Endpoint = addrPort.String()
+	}
+
+	go func() {
+		if err := app.RunWarp(ctx, l, opts); err != nil {
+			fatal(l, err)
+		}
+	}()
+
+	<-ctx.Done()
+
+	return nil
+}
